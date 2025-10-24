a3e312e8e41f604965ea236272b906b6a63ddaf3
Gustavo S. <ogustavone@gmail.com>
2025-10-24 16:47:21 -0300

    add images and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import Header from "@/Components/Header";
-import Banner from "@/Components/BannerVideo";
-import Contato from "@/Components/Contato";
+import Header from "@/components/Header";
+import Banner from "@/components/BannerVideo";
+import Contato from "@/components/Contato";
+import Sobre from "@/components/Sobre";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-gray-100 space-y-10">
       <Header />
       <Banner />
+      <Sobre/>
       <Contato/>
     </div>
   );
--- components/Carrossel.tsx
+++ components/Carrossel.tsx
@@ -13,7 +13,7 @@ export default function Carrossel() {
         {Array.from({ length: 4 }).map((_, index) => (
           <CarouselItem key={index}>
             <img
-              src={`/image-${index}.png`}
+              src={`igreja/image-${index+1}.png`}
               alt={`Figura ${index+1}`}
               className="object-center rounded-lg h-70 w-full object-cover"
             />
