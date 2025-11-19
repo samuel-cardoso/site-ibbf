e9b2b4e694f59f5e84224ce3f8b7f07a31fafdaa
Samuel <samuelcardosodev@gmail.com>
2025-11-18 21:19:30 -0300

    feat: add About component to homepage for church introduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/home/Navbar";
 import Hero from "@/components/home/Hero";
+import About from "@/components/home/About";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100 space-y-10">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
